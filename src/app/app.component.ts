4f05f5710dc9dd960bdccefe3e0a83a67babb8ad
leonardomuniz <leocontato@outlook.com.br>
2023-02-10 22:31:11 -0300

    Learning about angular life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-title title="E seu fizer uma brincadeira aqui"></app-title>
+    {{ valor }}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+export class AppComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    AfterViewChecked
+{
+  public valor: number = 1;
+
+  public adicionar(): number {
+    return (this.valor += 1);
+  }
+  ngOnInit(): void {
+    console.log('Ligar motores');
+  }
+
+  ngDoCheck(): void {
+    console.log('Checando propulsores');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Iniciando decolagem');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Estamos em pleno voo');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Iremos chegar no espaço em 3 . . . 2  . . . 1 . . .');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(
+      'Um pequeno paço para um homem mas um grande paço para a humandia'
+    );
+  }
 }
