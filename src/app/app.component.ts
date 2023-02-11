a770e422fdbacba65eb72ba68f384283af821baf
leonardomuniz <leocontato@outlook.com.br>
2023-02-10 22:14:51 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-title title="E seu fizer uma brincadeira aqui"></app-title>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
 }
